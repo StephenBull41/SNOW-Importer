4d822c1ec345eef1d215a76a1a1cab38315468a2
stephenbull41 <stephenbull41@gmail.com>
2019-10-14 14:30:53 +1100

    Update to first woring version

--- SNOW importer/Form1.Designer.cs
+++ SNOW importer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SNOW_importer
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rtb_import = new System.Windows.Forms.RichTextBox();
+            this.btn_import = new System.Windows.Forms.Button();
+            this.lbl_import_prompt = new System.Windows.Forms.Label();
+            this.btn_a_post = new System.Windows.Forms.Button();
+            this.btn_n_post = new System.Windows.Forms.Button();
+            this.btn_f_post = new System.Windows.Forms.Button();
+            this.lbl_site = new System.Windows.Forms.Label();
+            this.lbl_buid = new System.Windows.Forms.Label();
+            this.lbl_count = new System.Windows.Forms.Label();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.btn_export = new System.Windows.Forms.Button();
+            this.btn_export_status = new System.Windows.Forms.Button();
+            this.lbl_parent = new System.Windows.Forms.Label();
+            this.tbx_parent = new System.Windows.Forms.TextBox();
+            this.lbl_step = new System.Windows.Forms.Label();
+            this.lbl_step_stat = new System.Windows.Forms.Label();
+            this.lbl_date = new System.Windows.Forms.Label();
+            this.lbl_date_stat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtb_import
+            // 
+            this.rtb_import.DetectUrls = false;
+            this.rtb_import.Location = new System.Drawing.Point(12, 95);
+            this.rtb_import.Name = "rtb_import";
+            this.rtb_import.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.rtb_import.Size = new System.Drawing.Size(609, 354);
+            this.rtb_import.TabIndex = 0;
+            this.rtb_import.Text = "";
+            this.rtb_import.WordWrap = false;
+            // 
+            // btn_import
+            // 
+            this.btn_import.Location = new System.Drawing.Point(12, 12);
+            this.btn_import.Name = "btn_import";
+            this.btn_import.Size = new System.Drawing.Size(609, 54);
+            this.btn_import.TabIndex = 1;
+            this.btn_import.Text = "Import";
+            this.btn_import.UseVisualStyleBackColor = true;
+            this.btn_import.Click += new System.EventHandler(this.btn_import_Click);
+            // 
+            // lbl_import_prompt
+            // 
+            this.lbl_import_prompt.AutoSize = true;
+            this.lbl_import_prompt.Location = new System.Drawing.Point(12, 79);
+            this.lbl_import_prompt.Name = "lbl_import_prompt";
+            this.lbl_import_prompt.Size = new System.Drawing.Size(133, 13);
+            this.lbl_import_prompt.TabIndex = 2;
+            this.lbl_import_prompt.Text = "Paste text here then import";
+            // 
+            // btn_a_post
+            // 
+            this.btn_a_post.Location = new System.Drawing.Point(12, 310);
+            this.btn_a_post.Name = "btn_a_post";
+            this.btn_a_post.Size = new System.Drawing.Size(200, 117);
+            this.btn_a_post.TabIndex = 3;
+            this.btn_a_post.Text = "Already posted";
+            this.btn_a_post.UseVisualStyleBackColor = true;
+            this.btn_a_post.Click += new System.EventHandler(this.btn_a_post_Click);
+            // 
+            // btn_n_post
+            // 
+            this.btn_n_post.Location = new System.Drawing.Point(421, 310);
+            this.btn_n_post.Name = "btn_n_post";
+            this.btn_n_post.Size = new System.Drawing.Size(200, 117);
+            this.btn_n_post.TabIndex = 4;
+            this.btn_n_post.Text = "Post failed or pending";
+            this.btn_n_post.UseVisualStyleBackColor = true;
+            this.btn_n_post.Click += new System.EventHandler(this.btn_n_post_Click);
+            // 
+            // btn_f_post
+            // 
+            this.btn_f_post.Location = new System.Drawing.Point(216, 310);
+            this.btn_f_post.Name = "btn_f_post";
+            this.btn_f_post.Size = new System.Drawing.Size(200, 117);
+            this.btn_f_post.TabIndex = 5;
+            this.btn_f_post.Text = "Force posted";
+            this.btn_f_post.UseVisualStyleBackColor = true;
+            this.btn_f_post.Click += new System.EventHandler(this.btn_f_post_Click);
+            // 
+            // lbl_site
+            // 
+            this.lbl_site.AutoSize = true;
+            this.lbl_site.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_site.Location = new System.Drawing.Point(25, 156);
+            this.lbl_site.Name = "lbl_site";
+            this.lbl_site.Size = new System.Drawing.Size(98, 24);
+            this.lbl_site.TabIndex = 6;
+            this.lbl_site.Text = "Site BU ID:";
+            // 
+            // lbl_buid
+            // 
+            this.lbl_buid.AutoSize = true;
+            this.lbl_buid.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_buid.Location = new System.Drawing.Point(129, 156);
+            this.lbl_buid.Name = "lbl_buid";
+            this.lbl_buid.Size = new System.Drawing.Size(57, 24);
+            this.lbl_buid.TabIndex = 7;
+            this.lbl_buid.Text = " BUID";
+            // 
+            // lbl_count
+            // 
+            this.lbl_count.AutoSize = true;
+            this.lbl_count.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_count.Location = new System.Drawing.Point(531, 156);
+            this.lbl_count.Name = "lbl_count";
+            this.lbl_count.Size = new System.Drawing.Size(57, 24);
+            this.lbl_count.TabIndex = 8;
+            this.lbl_count.Text = "count";
+            // 
+            // btn_back
+            // 
+            this.btn_back.Location = new System.Drawing.Point(492, 156);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(42, 24);
+            this.btn_back.TabIndex = 9;
+            this.btn_back.Text = "<";
+            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // btn_export
+            // 
+            this.btn_export.Location = new System.Drawing.Point(12, 12);
+            this.btn_export.Name = "btn_export";
+            this.btn_export.Size = new System.Drawing.Size(301, 54);
+            this.btn_export.TabIndex = 10;
+            this.btn_export.Text = "Export SNOW import list";
+            this.btn_export.UseVisualStyleBackColor = true;
+            this.btn_export.Click += new System.EventHandler(this.btn_export_Click);
+            // 
+            // btn_export_status
+            // 
+            this.btn_export_status.Location = new System.Drawing.Point(321, 12);
+            this.btn_export_status.Name = "btn_export_status";
+            this.btn_export_status.Size = new System.Drawing.Size(301, 54);
+            this.btn_export_status.TabIndex = 11;
+            this.btn_export_status.Text = "Export job staus";
+            this.btn_export_status.UseVisualStyleBackColor = true;
+            this.btn_export_status.Click += new System.EventHandler(this.btn_export_status_Click);
+            // 
+            // lbl_parent
+            // 
+            this.lbl_parent.AutoSize = true;
+            this.lbl_parent.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_parent.Location = new System.Drawing.Point(25, 132);
+            this.lbl_parent.Name = "lbl_parent";
+            this.lbl_parent.Size = new System.Drawing.Size(69, 24);
+            this.lbl_parent.TabIndex = 12;
+            this.lbl_parent.Text = "Parent:";
+            // 
+            // tbx_parent
+            // 
+            this.tbx_parent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_parent.Location = new System.Drawing.Point(129, 132);
+            this.tbx_parent.Name = "tbx_parent";
+            this.tbx_parent.Size = new System.Drawing.Size(83, 23);
+            this.tbx_parent.TabIndex = 13;
+            this.tbx_parent.TextChanged += new System.EventHandler(this.tbx_parent_TextChanged);
+            // 
+            // lbl_step
+            // 
+            this.lbl_step.AutoSize = true;
+            this.lbl_step.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_step.Location = new System.Drawing.Point(129, 204);
+            this.lbl_step.Name = "lbl_step";
+            this.lbl_step.Size = new System.Drawing.Size(45, 24);
+            this.lbl_step.TabIndex = 15;
+            this.lbl_step.Text = "step";
+            // 
+            // lbl_step_stat
+            // 
+            this.lbl_step_stat.AutoSize = true;
+            this.lbl_step_stat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_step_stat.Location = new System.Drawing.Point(25, 204);
+            this.lbl_step_stat.Name = "lbl_step_stat";
+            this.lbl_step_stat.Size = new System.Drawing.Size(53, 24);
+            this.lbl_step_stat.TabIndex = 14;
+            this.lbl_step_stat.Text = "Step:";
+            // 
+            // lbl_date
+            // 
+            this.lbl_date.AutoSize = true;
+            this.lbl_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_date.Location = new System.Drawing.Point(129, 180);
+            this.lbl_date.Name = "lbl_date";
+            this.lbl_date.Size = new System.Drawing.Size(46, 24);
+            this.lbl_date.TabIndex = 17;
+            this.lbl_date.Text = "date";
+            // 
+            // lbl_date_stat
+            // 
+            this.lbl_date_stat.AutoSize = true;
+            this.lbl_date_stat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_date_stat.Location = new System.Drawing.Point(25, 180);
+            this.lbl_date_stat.Name = "lbl_date_stat";
+            this.lbl_date_stat.Size = new System.Drawing.Size(53, 24);
+            this.lbl_date_stat.TabIndex = 16;
+            this.lbl_date_stat.Text = "Date:";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(634, 461);
+            this.Controls.Add(this.lbl_date);
+            this.Controls.Add(this.lbl_date_stat);
+            this.Controls.Add(this.lbl_step);
+            this.Controls.Add(this.lbl_step_stat);
+            this.Controls.Add(this.tbx_parent);
+            this.Controls.Add(this.lbl_parent);
+            this.Controls.Add(this.btn_export_status);
+            this.Controls.Add(this.btn_export);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.lbl_count);
+            this.Controls.Add(this.lbl_buid);
+            this.Controls.Add(this.lbl_site);
+            this.Controls.Add(this.btn_f_post);
+            this.Controls.Add(this.btn_n_post);
+            this.Controls.Add(this.btn_a_post);
+            this.Controls.Add(this.lbl_import_prompt);
+            this.Controls.Add(this.btn_import);
+            this.Controls.Add(this.rtb_import);
+            this.MaximumSize = new System.Drawing.Size(650, 500);
+            this.MinimumSize = new System.Drawing.Size(650, 500);
+            this.Name = "Main";
+            this.Text = "StevesSuperSnowImporter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtb_import;
+        private System.Windows.Forms.Button btn_import;
+        private System.Windows.Forms.Label lbl_import_prompt;
+        private System.Windows.Forms.Button btn_a_post;
+        private System.Windows.Forms.Button btn_n_post;
+        private System.Windows.Forms.Button btn_f_post;
+        private System.Windows.Forms.Label lbl_site;
+        private System.Windows.Forms.Label lbl_buid;
+        private System.Windows.Forms.Label lbl_count;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Button btn_export;
+        private System.Windows.Forms.Button btn_export_status;
+        private System.Windows.Forms.Label lbl_parent;
+        private System.Windows.Forms.TextBox tbx_parent;
+        private System.Windows.Forms.Label lbl_step;
+        private System.Windows.Forms.Label lbl_step_stat;
+        private System.Windows.Forms.Label lbl_date;
+        private System.Windows.Forms.Label lbl_date_stat;
     }
 }
 
--- SNOW importer/Form1.cs
+++ SNOW importer/Form1.cs
@@ -1,20 +1,171 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace SNOW_importer
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public int site_index = 0;
+        public JJob[] jobs = new JJob[2];
+
+
+        public Main()
         {
             InitializeComponent();
+            lbl_step.Visible = lbl_step_stat.Visible = lbl_date.Visible = lbl_date_stat.Visible = lbl_parent.Visible = tbx_parent.Visible = btn_export_status.Visible = btn_export.Visible = btn_back.Visible = lbl_count.Visible = lbl_buid.Visible = lbl_site.Visible = btn_a_post.Visible = btn_f_post.Visible = btn_n_post.Visible = false;
         }
-    }
+
+        private void btn_import_Click(object sender, EventArgs e)
+        {
+
+            string[] import_arr = rtb_import.Lines;
+            int i = 0;
+            foreach(string j in import_arr)
+            {
+                if (j.Length > 1) { i++; }
+            }
+            JJob[] job_arr = new JJob[i];
+
+            i = 0;
+            rtb_import.Clear();
+            foreach (string job in import_arr)
+            {
+                if (job.Length > 1)
+                {
+
+                    JJob jj = new JJob();
+
+                    string id = job.Remove(0, 13).Remove(5);
+                    string job_c = job.Remove(0, 13);
+                    string buid = job_c.Remove(job_c.IndexOf('-', 8) + 8);
+                    string step = job.Split(' ')[job.Split(' ').Length -1];
+                    string bu_d = job.Remove(0, job.IndexOf('/') - 2).Remove(9);
+                    int idx = step.LastIndexOfAny(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+                    if(idx > 0) { step = step.Remove(0, idx + 1); }
+
+                    jj.buid = buid;
+                    jj.site_id = id;
+                    jj.bu_date = bu_d;
+                    jj.step = step;
+                    job_arr[i] = jj;
+                    i++;
+                }
+           
+            }
+
+            lbl_import_prompt.Visible = rtb_import.Visible = btn_import.Visible = false;
+            lbl_step.Visible = lbl_step_stat.Visible = lbl_date.Visible = lbl_date_stat.Visible = btn_export.Visible = lbl_parent.Visible = tbx_parent.Visible = lbl_count.Visible = lbl_buid.Visible = lbl_site.Visible = btn_a_post.Visible = btn_f_post.Visible = btn_n_post.Visible = true;
+
+            jobs = job_arr;
+            load_next();
+        }
+
+        private void load_next()
+        {
+            if(site_index < jobs.Length)
+            {
+                lbl_buid.Text = jobs[site_index].buid;
+                lbl_count.Text = (site_index + 1) + "/" + jobs.Length;
+                lbl_date.Text = jobs[site_index].bu_date;
+                lbl_step.Text = jobs[site_index].step;
+                Clipboard.SetText(jobs[site_index].buid);
+                site_index++;
+            }
+            else
+            {
+                btn_export_status.Visible = btn_export.Visible = true;
+            } 
+        }
+
+        private void btn_a_post_Click(object sender, EventArgs e)
+        {
+            if(site_index <= jobs.Length) { jobs[site_index - 1].status = "Already posted"; }  
+            load_next();
+        }
+
+        private void btn_f_post_Click(object sender, EventArgs e)
+        {
+            if (site_index <= jobs.Length) { jobs[site_index - 1].status = "Force posted"; } 
+            load_next();
+        }
+
+        private void btn_n_post_Click(object sender, EventArgs e)
+        {
+            if (site_index <= jobs.Length) { jobs[site_index - 1].status = "Not posted"; } 
+            load_next();
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            //not working
+            if (site_index > 1) { site_index--; }
+            load_next();
+        }
+
+        private void export_jobs(string parent)
+        {
+            string export_path = System.Reflection.Assembly.GetEntryAssembly().Location.Remove(System.Reflection.Assembly.GetEntryAssembly().Location.LastIndexOf("\\") + 1);
+            string[] out_arr_jobs = new string[jobs.Length + 1];
+            int i = 1;
+            out_arr_jobs[0] = "site,inherit from,notes";
+
+            foreach(JJob job in jobs)
+            {
+                out_arr_jobs[i] = $"{job.site_id},{parent},BU_Date:{job.bu_date} | Step:{job.step}";
+                i++;
+            }
+            
+            File.WriteAllLines((export_path + "jobs_out.csv"), out_arr_jobs);
+        }
+
+        private void btn_export_status_Click(object sender, EventArgs e)
+        {
+            string export_path = System.Reflection.Assembly.GetEntryAssembly().Location.Remove(System.Reflection.Assembly.GetEntryAssembly().Location.LastIndexOf("\\") + 1);
+            string[] out_arr_status = new string[jobs.Length + 1];
+            int i = 1;
+            out_arr_status[0] = "BUID - STATUS";
+
+            foreach (JJob job in jobs)
+            {
+                out_arr_status[i] = job.buid + " - " + job.status;
+                i++;
+            }
+
+            File.WriteAllLines((export_path + "status_out.txt"), out_arr_status);
+        }
+
+        private void btn_export_Click(object sender, EventArgs e)
+        {
+            if(tbx_parent.Text.Length == 10)
+            {
+                export_jobs(tbx_parent.Text.ToUpper());
+                btn_export.Visible = false;
+            }
+            else
+            {
+                btn_export.Text = "Enter a valid parent incident";
+            }            
+        }
+
+        private void tbx_parent_TextChanged(object sender, EventArgs e)
+        {
+            if (tbx_parent.Text.Length == 10)
+            {
+                btn_export.Text = "Export SNOW import list";
+            }
+        }
+    }   
+}
+
+public class JJob
+{
+    public string site_id = string.Empty;
+    public string buid = string.Empty;
+    public string status = string.Empty;
+    public string bu_date = string.Empty;
+    public string step = string.Empty;
 }
